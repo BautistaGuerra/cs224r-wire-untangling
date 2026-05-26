601b46c305985069617d75a0a9b614fcd8c35137
BautistaGuerra <bauguerra@gmail.com>
2026-05-25 17:51:03 -0700

    revert: remove learned context predictor from dpfm branch

--- wire_untangling/policies/__init__.py
+++ wire_untangling/policies/__init__.py
@@ -1,4 +1,3 @@
-from wire_untangling.policies.context_predictor import ContextPredictor
 from wire_untangling.policies.pick_place_expert import PickPlaceExpertPolicy, build_obs_index_map
 
-__all__ = ["ContextPredictor", "PickPlaceExpertPolicy", "build_obs_index_map"]
+__all__ = ["PickPlaceExpertPolicy", "build_obs_index_map"]
